a704a3419f291bc07a0c48629b077ecb9efa8c91
jmwania <josphatmwania237@gmail.com>
2019-02-25 12:24:48 +0300

    [25022019] Bank_account

--- bank_account.py
+++ bank_account.py
@@ -1,13 +1,15 @@
 class BankAccount(object):
     def __init__(self, balance=5000):
         self.balance = balance
-    
+        
+    # deposting method
     def deposit(self, deposit):
         self.balance+=deposit
         return self.balance
     
+    # withdrawing method
     def withdraw(self, withdraw):
-        if withdraw <=self.self.balance:
+        if withdraw <=self.balance:
             self.balance -= withdraw
             return self.balance
 
@@ -18,6 +20,6 @@ class Subclass(BankAccount):
     def __init__(self, minimum):
         self.minimum= minimum
 first = BankAccount()
-#print(first.deposit(3000))
+print(first.deposit(3000))
 print(first.withdraw(1000))
     
--- test_bank_account.py
+++ test_bank_account.py
@@ -1,19 +1,23 @@
 import unittest
-from bank_account import BankAccount, subclass
+from bank_acoount import BankAccount, Subclass
 
 class TestBankAccount(unittest.TestCase):
     def setUp(self):
         self.my_account = BankAccount()
     
     def test_Balance(self):
-        self.assertEqual(self.my_account, 500, msg='Not able to retrieveaccount balance')
+        self.assertEqual(self.my_account.balance, 5000, msg='Not able to retrieveaccount balance')
+        
     def test_deposit(self):
         self.my_account.deposit(3000)
-        self.assertEqual(self.my_account.balance, 8000, msg='Inncorec deposit')
+        self.assertEqual(self.my_account.balance, 8000, msg='Inncorect deposit')
 
     def test_withdraw(self):
-        self.my_account.withdraw(1000)
+        self.my_account.withdraw(7000)
         self.assertEqual(self.my_account.balance, 4000, msg='Too much withdraw')
 
     def test_subclass(self):
-        self.assertTrue(issubclass(SubClass, BankAccount), msg='Not a subclass')
+        self.assertTrue(issubclass(Subclass, BankAccount), msg='Not a subclass')
+
+if __name__ == "__main__":
+    unittest.main()
