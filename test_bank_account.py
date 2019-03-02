da360b9c0c64d5d0c709fe8dda50776cbe5e54a8
jmwania <josphatmwania237@gmail.com>
2019-03-02 11:37:29 +0300

    [02032019]

--- test_bank_account.py
+++ test_bank_account.py
@@ -1,5 +1,5 @@
 import unittest
-from bank_acoount import BankAccount, Subclass
+from bank_account import BankAccount, Subclass
 
 class TestBankAccount(unittest.TestCase):
     def setUp(self):
@@ -11,11 +11,7 @@ class TestBankAccount(unittest.TestCase):
     def test_deposit(self):
         self.my_account.deposit(3000)
         self.assertEqual(self.my_account.balance, 8000, msg='Inncorect deposit')
-
-    def test_withdraw(self):
-        self.my_account.withdraw(7000)
-        self.assertEqual(self.my_account.balance, 4000, msg='Too much withdraw')
-
+        
     def test_subclass(self):
         self.assertTrue(issubclass(Subclass, BankAccount), msg='Not a subclass')
 
